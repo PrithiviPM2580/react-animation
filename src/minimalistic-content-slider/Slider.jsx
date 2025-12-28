9b1f02fdbc252b4d314a47c3267fe7731c3c29fa
Prithivi <prithivi@users.noreply.github.com>
2025-12-28 23:03:08 +0545

    feat: implement small and big slider functionality with GSAP animations

--- src/minimalistic-content-slider/Slider.jsx
+++ src/minimalistic-content-slider/Slider.jsx
@@ -15,23 +15,69 @@ const sentences = [
 const Slider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const totalSlides = sentences.length;
-  const slidesRef = useRef(null);
+  const textSlideRef = useRef(null);
+  const smallSliderRef = useRef(null);
+  const bigSliderRef = useRef(null);
 
   useGSAP(() => {
-    gsap.to(slidesRef.current, {
+    gsap.to(textSlideRef.current, {
       x: -100 * currentIndex + "vw",
       duration: 0.5,
       ease: "power1.out",
       overwrite: "auto",
     });
+    gsap.fromTo(
+      smallSliderRef.current,
+      {
+        scale: 0.95,
+      },
+      {
+        x: -20 * currentIndex + "vw",
+        scale: 1,
+        duration: 0.5,
+        ease: "power1.out",
+        overwrite: "auto",
+      }
+    );
+    gsap.fromTo(
+      bigSliderRef.current,
+      {
+        scale: 0.95,
+      },
+      {
+        x: -50 * currentIndex + "vw",
+        scale: 1,
+        duration: 0.5,
+        ease: "power1.out",
+        overwrite: "auto",
+      }
+    );
   }, [currentIndex]);
 
   return (
     <>
       <div className="small-slider">
-        <div className="small-slides" ref={slidesRef}>
+        <div className="small-slides" ref={smallSliderRef}>
           {Array.from({ length: totalSlides }).map((_, index) => (
-            <div className="img" key={index}>
+            <div
+              className={`img ${index === currentIndex ? "active" : ""}`}
+              key={index}
+            >
+              <img
+                src={`./images/img${index + 1}.webp`}
+                alt={`Image ${index + 1}`}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="big-slider">
+        <div className="big-slides" ref={bigSliderRef}>
+          {Array.from({ length: totalSlides }).map((_, index) => (
+            <div
+              className={`img ${index === currentIndex ? "active" : ""}`}
+              key={index}
+            >
               <img
                 src={`./images/img${index + 1}.webp`}
                 alt={`Image ${index + 1}`}
@@ -41,7 +87,7 @@ const Slider = () => {
         </div>
       </div>
       <div className="text-slider">
-        <div className="text-slides" ref={slidesRef}>
+        <div className="text-slides" ref={textSlideRef}>
           {sentences.map((sentence, index) => (
             <div
               className={`text-slide ${index === currentIndex ? "active" : ""}`}
