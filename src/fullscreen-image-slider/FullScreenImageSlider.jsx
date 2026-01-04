66699ee4c8d63d9f36039566dbfa4c418875bf3f
Prithivi <prithivi@users.noreply.github.com>
2026-01-04 23:22:57 +0545

    feat: implement FullScreenImageSlider component with slider content and styles; add associated CSS file

--- src/fullscreen-image-slider/FullScreenImageSlider.jsx
+++ src/fullscreen-image-slider/FullScreenImageSlider.jsx
@@ -1,5 +1,50 @@
 const FullScreenImageSlider = () => {
-  return <div>FullScreenImageSlider</div>;
+  return (
+    <>
+      <div className="slider-content">
+        <div className="slide-number">
+          <div className="prefix">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <div key={index}>{index + 1}</div>
+            ))}
+          </div>
+          <div className="postfix">
+            <span>/</span> 5
+          </div>
+        </div>
+        <div className="slide-name">
+          <div className="names">
+            {[
+              "Ether Shift Mode",
+              "Solar Thread",
+              "Quantam Sheil Veil",
+              "Flux Aura",
+              "Echo Nimbus",
+            ].map((name, index) => (
+              <div key={index}>{name}</div>
+            ))}
+          </div>
+        </div>
+        <div className="slide-year">
+          <div className="years">
+            {["2021", "2022", "2023", "2024", "2025"].map((year, index) => (
+              <div key={index}>{year}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="slider">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <div className="slide" id={`slide-${index + 1}`}>
+            <img
+              src={`./images/img${index + 1}.png`}
+              alt={`Slide ${index + 1}`}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default FullScreenImageSlider;
