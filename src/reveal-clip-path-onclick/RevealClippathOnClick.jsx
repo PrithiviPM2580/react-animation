4d6a06995ef4883c7d739fbb5b0d0657beeda3bb
Prithivi <prithivi@users.noreply.github.com>
2026-01-20 18:54:18 +0545

    feat: implement animation functionality in RevealClippathOnClick component with associated styles

--- src/reveal-clip-path-onclick/RevealClippathOnClick.jsx
+++ src/reveal-clip-path-onclick/RevealClippathOnClick.jsx
@@ -1,9 +1,48 @@
+import { useGSAP } from "@gsap/react";
 import "./index.css";
+import { useRef } from "react";
+import gsap from "gsap";
 
 const RevealClippathOnClick = () => {
+  const tl = useRef(null);
+  const containerRef = useRef(null);
+  const overlayRef = useRef(null);
+
+  useGSAP(
+    () => {
+      tl.current = gsap
+        .timeline({
+          paused: true,
+          defaults: { ease: "power4.inOut" },
+        })
+        .to(overlayRef.current, {
+          clipPath: "polygon(0% 0%,100% 0%,100% 0%,0% 0%)",
+          duration: 2,
+        })
+        .to(
+          ".img",
+          {
+            clipPath: "polygon(0% 100%,100% 100%,100% 0%,0% 0%)",
+            duration: 1,
+            stagger: { amount: 1.5 },
+          },
+          "-=1.4",
+        )
+        .to(
+          ".loader",
+          { clipPath: "polygon(0% 0%,100% 0%,100% 0%,0% 0%)", duration: 1 },
+          "-=0.7",
+        );
+    },
+    { scope: containerRef },
+  );
+
+  const handleAnimationClick = () => {
+    tl.current.play();
+  };
   return (
     <>
-      <div className="container">
+      <div className="container" ref={containerRef}>
         <nav>
           <div className="logo">PPM</div>
           <div className="navs">
@@ -15,7 +54,7 @@ const RevealClippathOnClick = () => {
         <h1>Welcome to PPM</h1>
         <div className="loader">
           {Array.from({ length: 6 }).map((_, index) => (
-            <div className="img">
+            <div className="img" key={index}>
               <img
                 src={`../images/img${index + 1}.png`}
                 alt={`Image ${index + 1}`}
@@ -23,6 +62,9 @@ const RevealClippathOnClick = () => {
             </div>
           ))}
         </div>
+      </div>
+      <div className="overlay" ref={overlayRef}>
+        <button onClick={handleAnimationClick}>Click Anywhere</button>
       </div>
     </>
   );
