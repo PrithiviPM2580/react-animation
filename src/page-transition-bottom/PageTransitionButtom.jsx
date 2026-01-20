940996faccf96e2b52bf51bed590563962c8e455
Prithivi <prithivi@users.noreply.github.com>
2026-01-20 14:06:28 +0545

    feat: add PageTransitionButtom component with routing functionality

---
+++ src/page-transition-bottom/PageTransitionButtom.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import PageTransitionRoutes from "./PageTransitionRoutes";
+import "./index.css";
+
+const PageTransitionButtom = () => {
+  return <PageTransitionRoutes />;
+};
+
+export default PageTransitionButtom;
