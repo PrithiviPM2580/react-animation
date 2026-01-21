6833e77f168c6c13f77ad821feb3b491138c09c7
Prithivi <prithivi@users.noreply.github.com>
2026-01-21 13:36:22 +0545

    feat: refactor HoverTransition component to use itemData for dynamic image rendering

--- src/image-transition-onclick/HoverTransition.jsx
+++ src/image-transition-onclick/HoverTransition.jsx
@@ -1,30 +1,31 @@
-import React from "react";
 import { useState } from "react";
 
-const textData = [
-  "Azure",
-  "Euphoria",
-  "Serendipity",
-  "Luminous",
-  "Ephemeral",
-  "Solitude",
+const itemData = [
+  { text: "Azure", img: "./images/img1.png" },
+  { text: "Euphoria", img: "./images/img2.png" },
+  { text: "Serendipity", img: "./images/img3.png" },
+  { text: "Luminous", img: "./images/img4.png" },
+  { text: "Ephemeral", img: "./images/img5.png" },
 ];
-
 const HoverTransition = () => {
-  const [activeImg, setActiveImg] = useState();
+  const [activeImg, setActiveImg] = useState(itemData[0].img);
   return (
     <div className="img-container">
       <div className="text-container">
-        {textData.map((text, index) => (
-          <div className="text" key={index}>
-            <p>{text}</p>
+        {itemData.map((item, index) => (
+          <div
+            className="text"
+            key={index}
+            onMouseEnter={() => setActiveImg(item.img)}
+          >
+            <p>{item.text}</p>
           </div>
         ))}
       </div>
 
       <div className="image-container">
         <div className="img">
-          <img src="./images/img1.png" alt="" />
+          <img src={activeImg} alt="" />
         </div>
       </div>
     </div>
