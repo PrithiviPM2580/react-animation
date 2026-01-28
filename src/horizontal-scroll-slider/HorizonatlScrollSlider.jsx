7f1f6617bc1a2493a3e2ba80af85a2c12fd393cf
Prithivi <prithivi@users.noreply.github.com>
2026-01-28 21:31:21 +0545

    feat: enhance HorizonatlScrollSlider with GSAP animations and responsive design

--- src/horizontal-scroll-slider/HorizonatlScrollSlider.jsx
+++ src/horizontal-scroll-slider/HorizonatlScrollSlider.jsx
@@ -1,8 +1,62 @@
+import gsap from "gsap";
 import "./index.css";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 const HorizonatlScrollSlider = () => {
+  const containerRef = useRef(null);
+  const currentX = useRef(0);
+  const targetX = useRef(0);
+  const sliderRef = useRef(null);
+  const maxScrollRef = useRef(0);
+  const horizontaLScrollXRef = useRef(null);
+
+  const lerp = (start, end, amt) => {
+    return (1 - amt) * start + amt * end;
+  };
+
+  function update() {
+    currentX.current = lerp(currentX.current, targetX.current, 0.1);
+    if (horizontaLScrollXRef.current) {
+      horizontaLScrollXRef.current(-currentX.current);
+    }
+
+    requestAnimationFrame(update);
+  }
+
+  function handleWheel(e) {
+    targetX.current += e.deltaY;
+
+    targetX.current = Math.max(0, targetX.current);
+    targetX.current = Math.min(maxScrollRef.current, targetX.current);
+  }
+
+  function handleResize() {
+    if (maxScrollRef.current) {
+      maxScrollRef.current = sliderRef.current.offsetWidth - window.innerWidth;
+    }
+  }
+  requestAnimationFrame(update);
+
+  useGSAP(
+    () => {
+      if (sliderRef.current) {
+        maxScrollRef.current =
+          sliderRef.current.offsetWidth - window.innerWidth;
+        horizontaLScrollXRef.current = gsap.quickTo(sliderRef.current, "x");
+      }
+      window.addEventListener("wheel", (e) => handleWheel(e));
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("wheel", (e) => handleWheel(e));
+        window.removeEventListener("resize", handleResize);
+      };
+    },
+    { scope: containerRef },
+  );
   return (
-    <>
+    <div className="container" ref={containerRef}>
       <nav>
         <div>Logo</div>
         <div>Menu</div>
@@ -15,22 +69,22 @@ const HorizonatlScrollSlider = () => {
       </div>
 
       <div className="slider">
-        <div className="slider-wrapper">
-          {[...Array(10)].map((_, index) => (
-            <div className="slide">
-              <img
-                src={`./images/img${index + 1}.png`}
-                alt={`Slide ${index + 1}`}
-              />
-            </div>
-          ))}
+        <div className="slider-wrapper" ref={sliderRef}>
+          {[...Array(10)].map((_, index) => {
+            const i = (index % 6) + 1;
+            return (
+              <div className="slide" key={index}>
+                <img src={`./images/img${i}.png`} alt={`Slide ${i}`} />
+              </div>
+            );
+          })}
         </div>
       </div>
       <footer>
         <p>PPM 2026 &copy;</p>
         <p>Creative Team</p>
       </footer>
-    </>
+    </div>
   );
 };
 
