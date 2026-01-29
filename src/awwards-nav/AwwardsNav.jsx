89af2d01ddd15d0116f17ad8c8d52605579e7124
Prithivi <prithivi@users.noreply.github.com>
2026-01-29 13:16:29 +0545

    feat: enhance AwwardsNav component with GSAP animations and button click functionality

--- src/awwards-nav/AwwardsNav.jsx
+++ src/awwards-nav/AwwardsNav.jsx
@@ -1,15 +1,39 @@
+import { useGSAP } from "@gsap/react";
 import "./index.css";
+import { useRef } from "react";
+import gsap from "gsap";
 
 const AwwardsNav = () => {
+  const containerRef = useRef(null);
+  const tl = useRef(null);
+
+  useGSAP(
+    () => {
+      tl.current = gsap.timeline({ paused: true }).to(".nav", {
+        height: "50vh",
+      });
+    },
+    { scope: containerRef },
+  );
+
+  const handleScaleClick = () => {
+    console.log("clicked");
+    tl.current.reversed(!tl.current.reversed());
+  };
   return (
-    <div className="container">
+    <div className="container" ref={containerRef}>
       <div className="nav">
         <div className="navs"></div>
         <div className="btns">
           {["More", "Home", "Nominees", "Directory", "Collection"].map(
             (btnText, index) => {
               return (
-                <button key={index} className="btn" id={`btn-${index + 1}`}>
+                <button
+                  key={index}
+                  className="btn"
+                  id={`btn-${index + 1}`}
+                  onClick={index === 0 ? handleScaleClick : undefined}
+                >
                   {" "}
                   {index === 0 ? <span>&#9776;</span> : null}
                   {btnText}
