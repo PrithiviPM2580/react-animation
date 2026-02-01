ce1eeb1969ee9809f99464924bc053810b2cf046
Prithivi <prithivi@users.noreply.github.com>
2026-02-01 21:53:31 +0545

    feat: refactor ParallexImageMouseMove component for improved mouse interaction and styling

--- src/parallex-image-mousemove/ParallexImageMouseMove.jsx
+++ src/parallex-image-mousemove/ParallexImageMouseMove.jsx
@@ -1,37 +1,43 @@
+import { useGSAP } from "@gsap/react";
 import "./index.css";
+import { useRef } from "react";
+import gsap from "gsap";
 
 const ParallexImageMouseMove = () => {
-  // Define grid positions for 5x5 grid (25 items total)
-  const positions = [
-    { col: 1, row: 1 },
-    { col: 1, row: 2 },
-    { col: 1, row: 3 },
-    { col: 1, row: 4 },
-    { col: 1, row: 5 },
-    { col: 2, row: 1 },
-    { col: 2, row: 2 },
-    { col: 2, row: 3 },
-    { col: 2, row: 4 },
-    { col: 2, row: 5 },
-    { col: 3, row: 1 },
-    { col: 3, row: 2 },
-    { col: 3, row: 3 },
-    { col: 3, row: 4 },
-    { col: 3, row: 5 },
-    { col: 4, row: 1 },
-    { col: 4, row: 2 },
-    { col: 4, row: 3 },
-    { col: 4, row: 4 },
-    { col: 4, row: 5 },
-    { col: 5, row: 1 },
-    { col: 5, row: 2 },
-    { col: 5, row: 3 },
-    { col: 5, row: 4 },
-    { col: 5, row: 5 },
-  ];
+  const containerRef = useRef(null);
+  const parallelSpeed = 10;
 
+  useGSAP(
+    () => {
+      const items = gsap.utils.toArray(".item");
+
+      const handleMouseMove = (e) => {
+        const x = (e.clientX / window.innerWidth - 0.5) * 2;
+        const y = (e.clientY / window.innerHeight - 0.5) * 2;
+
+        items.forEach((item, index) => {
+          // Each item gets a different speed multiplier (1x, 2x, 3x, etc.)
+          const speedMultiplier = (index + 1) * 0.5;
+
+          gsap.to(item, {
+            x: x * parallelSpeed * speedMultiplier,
+            y: y * parallelSpeed * speedMultiplier,
+            ease: "power2.out",
+            duration: 0.6,
+          });
+        });
+      };
+
+      window.addEventListener("mousemove", handleMouseMove);
+
+      return () => {
+        window.removeEventListener("mousemove", handleMouseMove);
+      };
+    },
+    { scope: containerRef },
+  );
   return (
-    <div className="container">
+    <div className="container" ref={containerRef}>
       <div className="header">
         <h1>Parallex Image Mouse Move</h1>
         <p>Move your mouse over the image to see the parallax effect.</p>
@@ -40,15 +46,15 @@ const ParallexImageMouseMove = () => {
         </div>
       </div>
       <div className="gallery">
-        {positions.map((pos, i) => {
+        {[...Array(15)].map((_, i) => {
           const index = (i % 6) + 1;
           return (
             <div
               className="item"
               key={i}
               style={{
-                gridColumn: pos.col,
-                gridRow: pos.row,
+                top: Math.random() * 80 + "%",
+                left: Math.random() * 80 + "%",
               }}
             >
               <img src={`./images/img${index}.png`} alt={`Image ${index}`} />
