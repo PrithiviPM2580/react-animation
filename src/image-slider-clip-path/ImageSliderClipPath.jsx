235387b6a58c31f757fec33bea6ad963b8d48293
Prithivi <prithivi@users.noreply.github.com>
2026-03-10 12:22:55 +0545

    feat: implement GSAP animations for ImageSliderClipPath component with wheel scroll interaction

--- src/image-slider-clip-path/ImageSliderClipPath.jsx
+++ src/image-slider-clip-path/ImageSliderClipPath.jsx
@@ -1,9 +1,77 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import "./style.css";
+import { useRef } from "react";
 
 const ImageSliderClipPath = () => {
+  const sliderContentRef = useRef(null);
+  const slides = useRef([]);
+  const currentIndex = useRef(0);
+  const isAnimating = useRef(false);
+
+  function handleWheelScroll(e) {
+    e.preventDefault();
+    if (isAnimating.current) return;
+
+    const direction = e.deltaY > 0 ? 1 : -1;
+    const nextIndex = currentIndex.current + direction;
+
+    if (nextIndex < 0 || nextIndex >= slides.current.length) return;
+
+    isAnimating.current = true;
+
+    const tl = gsap.timeline({
+      defaults: { ease: "power4.inOut" },
+      onComplete: () => {
+        currentIndex.current = nextIndex;
+        isAnimating.current = false;
+      },
+    });
+
+    tl.to(".prefix, .names, .years", {
+      y: `+=${direction > 0 ? -30 : 30}`,
+      duration: 1,
+    });
+
+    slides.current.forEach((slide, index) => {
+      tl.to(slide, {
+        scale: 1,
+        top: "0%",
+        duration: 2,
+        ease: "power3.inOut",
+      })
+        .to(
+          slide,
+          {
+            clipPath:
+              index === nextIndex
+                ? "polygon(0 0, 100% 0, 100% 100%, 0 100%)"
+                : "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)",
+            duration: 1.2,
+          },
+          "<",
+        )
+        .to(
+          slide,
+          {
+            clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)",
+            duration: 1.2,
+          },
+          "<",
+        );
+    });
+  }
+
+  useGSAP(
+    () => {
+      window.addEventListener("wheel", handleWheelScroll, { passive: false });
+    },
+    { scope: sliderContentRef },
+  );
+
   return (
     <>
-      <div className="slider-content">
+      <div className="slider-content" ref={sliderContentRef}>
         <div className="slide-number">
           <div className="prefix">
             <div>1</div>
@@ -37,7 +105,12 @@ const ImageSliderClipPath = () => {
       </div>
       <div className="slider">
         {Array.from({ length: 5 }).map((_, index) => (
-          <div className="slide" key={index} id={`slide-${index + 1}`}>
+          <div
+            className="slide"
+            key={index}
+            id={`slide-${index + 1}`}
+            ref={(el) => (slides.current[index] = el)}
+          >
             <img
               src={`/images/img${index + 1}.png`}
               alt={`Image ${index + 1}`}
