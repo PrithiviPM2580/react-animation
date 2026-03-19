dd4d0c742736576bda7dfde09f4d7d1ab2fff960
Prithivi <prithivi@users.noreply.github.com>
2026-03-19 19:41:07 +0545

    feat: add Images and Minimap components with styling for MinimapScroll

--- src/minimap-scroll/MinimapScroll.jsx
+++ src/minimap-scroll/MinimapScroll.jsx
@@ -1,5 +1,40 @@
+import Images from "./Images";
+import Minimap from "./Minimap";
+import "./style.css";
+
 const MinimapScroll = () => {
-  return <div>MinimapScroll</div>;
+  return (
+    <div className="container">
+      <nav>
+        <p>PPM</p>
+        <button>Sign Up</button>
+      </nav>
+      <div className="section">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aut
+          officiis ex architecto perferendis minima eaque autem et. Possimus
+          consectetur nulla corporis excepturi dolor dignissimos sunt fugiat
+          ducimus! Consequuntur, ut.
+        </h1>
+      </div>
+      <div className="gallery">
+        {Array.from({ length: 10 }).map((_index) => (
+          <>
+            <Minimap />
+            <Images />
+          </>
+        ))}
+      </div>
+      <div className="section">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aut
+          officiis ex architecto perferendis minima eaque autem et. Possimus
+          consectetur nulla corporis excepturi dolor dignissimos sunt fugiat
+          ducimus! Consequuntur, ut.
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default MinimapScroll;
